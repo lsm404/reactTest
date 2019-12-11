c56bec2e9f37dfb755d433dbe9f71106a0f526dc
lee <shengminfang@foxmail.com>
2019-12-11 18:05:15 +0800

    content内容编写

--- src/pages/home/index.js
+++ src/pages/home/index.js
@@ -3,13 +3,23 @@ import { connect } from 'react-redux';
 // import { actionCreators } from './store';
 
 import {
-    HomeWrapper
+    HomeWrapper,
+    HomeLeft,
+    FirstItem
 } from './style'
 
 class Home extends PureComponent {
     render () {
         return (
-            <HomeWrapper>github建立仓库，测试提交代码</HomeWrapper>
+            <HomeWrapper>
+                
+                <HomeLeft>
+                     <img className='banner-img' alt='' src="//upload.jianshu.io/admin_banners/web_images/4318/60781ff21df1d1b03f5f8459e4a1983c009175a5.jpg?imageMogr2/auto-orient/strip|imageView2/1/w/1250/h/540" />
+                </HomeLeft>
+                <FirstItem>
+                    github建立仓库，测试提交代码
+                </FirstItem>
+            </HomeWrapper>
         )
     }
 }
--- src/pages/home/style.js
+++ src/pages/home/style.js
@@ -5,4 +5,22 @@ export const HomeWrapper = style.div`
     width: 960px;
     margin: 0 auto;
     text-align: center;
+`;
+
+export const HomeLeft = style.div`
+    width: 960px;
+    text-align: center;
+    padding-top: 30px;
+    .banner-img {
+        width: 625px;
+        height: 270px;
+    }
+`
+
+export const FirstItem = style.div`
+    width: 960px;
+    // float: left;
+    text-align: center;
+    height: 30px;
+    line-height: 30px;
 `
